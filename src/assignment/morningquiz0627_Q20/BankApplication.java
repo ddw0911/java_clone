44dafd5251201b3252567612fe7a004ed31d77cb
Dongwoo Jang <ddokkochi@gmail.com>
2024-07-09 19:16:50 +0900

    0709 MorningQuiz Account 리팩토링 /w 람다, 예외처리

--- src/assignment/morningquiz0627_Q20/BankApplication.java
+++ src/assignment/morningquiz0627_Q20/BankApplication.java
@@ -6,7 +6,7 @@ class BankApplication extends Account {
 
   Scanner sc = new Scanner(System.in);
 
-  Account[] accounts = new Account[100]; // 관리 계좌는 총 100개
+  final static Account[] accounts = new Account[100]; // 관리 계좌는 총 100개
   int currentAccCount = 0; // 현재 계좌 수
 
   public BankApplication() {
@@ -40,29 +40,29 @@ class BankApplication extends Account {
 
   void openAccount() { // 계좌생성
 
-    if (currentAccCount >= 100) { // 관리계좌 수를 넘기면 계좌생성불가
-      System.out.println("더 이상 계좌를 생성할 수 없습니다.");
-      return;
+    try {
+      InsufficientException.overAccountException(currentAccCount);
+      System.out.println("----------------");
+      System.out.println("계좌생성");
+      System.out.println("----------------");
+
+      System.out.print("계좌번호: ");
+      accNum = sc.nextLine();
+
+      System.out.print("계좌주: ");
+      accOwner = sc.nextLine();
+
+      System.out.print("초기입금액: ");
+      balance = sc.nextInt();
+      sc.nextLine();
+
+      accounts[currentAccCount] = new Account(accNum, accOwner, balance); // 입력받은 값을 관리계좌로 등록
+      currentAccCount++;
+
+      System.out.println("결과: 계좌가 생성되었습니다.");
+    } catch (InsufficientException e) {
+      System.out.println(e.getMessage());
     }
-
-    System.out.println("----------------");
-    System.out.println("계좌생성");
-    System.out.println("----------------");
-
-    System.out.print("계좌번호: ");
-    accNum = sc.nextLine();
-
-    System.out.print("계좌주: ");
-    accOwner = sc.nextLine();
-
-    System.out.print("초기입금액: ");
-    balance = sc.nextInt();
-    sc.nextLine();
-
-    accounts[currentAccCount] = new Account(accNum, accOwner, balance); // 입력받은 값을 관리계좌로 등록
-    currentAccCount++;
-
-    System.out.println("결과: 계좌가 생성되었습니다.");
   }
 
   void listAccount() { // 계좌목록
@@ -80,15 +80,24 @@ class BankApplication extends Account {
     System.out.println("----------------");
     System.out.print("계좌번호: ");
     accNum = sc.nextLine();
-    for (int i = 0; i < currentAccCount; i++) {
-      if (accNum.equals(accounts[i].accNum)) { // 등록된 계좌번호인지 확인
-        System.out.print("예금액: ");
-        accounts[i].balance += sc.nextInt(); // 해당 계좌에 입금
-        sc.nextLine();
-        break;
-      } else if (i == currentAccCount-1){ // 등록된 계좌가 아닐경우
-        System.out.println("없는 계좌번호입니다.");
+
+    try {
+      InsufficientException.isSignInAccount(accNum);
+      int i = 0;
+      while (i < accounts.length) {
+        if (accNum.equals(BankApplication.accounts[i].accNum)) {
+          System.out.print("예금액: ");
+          accounts[i].balance += sc.nextInt(); // 해당 계좌에 입금
+          sc.nextLine();
+          System.out.println("잔액: " + accounts[i].balance);
+          break;
+        }
+        i++;
       }
+    } catch (InsufficientException e) {
+      System.out.println(e.getMessage());
+    } catch (NullPointerException e) {
+      System.out.println("없는 계좌입니다.");
     }
   }
 
@@ -98,21 +107,31 @@ class BankApplication extends Account {
     System.out.println("----------------");
     System.out.print("계좌번호: ");
     accNum = sc.nextLine();
-    for (int i = 0; i < currentAccCount; i++) {
-      if (accNum.equals(accounts[i].accNum)) { // 등록된 계좌번호인지 확인
-        System.out.print("출금액: ");
-        int withdrawalAmount= sc.nextInt();
-        sc.nextLine();
-        if (withdrawalAmount > balance) { // 잔액보다 출금액이 크면
-          System.out.println("잔액이 부족합니다.");
-        } else {
-          accounts[i].balance -= withdrawalAmount; // 해당 계좌에서 출금
-          System.out.println("결과: 출금이 성공되었습니다.");
-          break;
+
+    try {
+      InsufficientException.isSignInAccount(accNum);
+      int i = 0;
+      while (i < accounts.length) {
+        if (accNum.equals(BankApplication.accounts[i].accNum)) {
+          System.out.print("출금액: ");
+          int withdrawalAmount = sc.nextInt();
+          sc.nextLine();
+
+          try {
+            InsufficientException.isAvailableWithdraw(withdrawalAmount, this.balance);
+            accounts[i].balance -= withdrawalAmount; // 해당 계좌에서 출금
+            System.out.println("결과: 출금이 성공되었습니다.");
+            System.out.println("잔액: " + accounts[i].balance);
+            break;
+          } catch (InsufficientException e) {
+            System.out.println(e.getMessage());
+          }
         }
-      } else if (i == currentAccCount-1){
-        System.out.println("없는 계좌번호입니다.");
+        i++;
       }
+    } catch (InsufficientException e) {
+      System.out.println(e.getMessage());
+    } catch (NullPointerException e) {
     }
   }
 
